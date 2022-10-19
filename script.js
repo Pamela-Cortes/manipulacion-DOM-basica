707d3f014c2251a566d951e4e5b4bc31b96517ff
Pamela Cortés <pamelacortes@MacBook-Pro-de-Pamela.local>
2022-10-19 13:02:58 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector ("h1");
+const p = documente.querySelector ("p");
+const parrafito = documen.querySelector (".parrafito");
+const pid = documen.querySelector ("#pid");
+const input = documen.querySelector ("input");
+
+console.log(input.value);
+
+console.log({
+h1,
+p,
+parrafito,
+pid,
+input,
+});
